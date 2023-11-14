b2f1a4893cf92505b7c057eff5ec3f03d9348c7b
FERNANDO JOSÉ PEREIRA DE SOUSA <FERNANDO0156@DIGITALCOLLEGE.LOCAL>
2023-11-13 21:45:54 -0300

    Cont.. Intro

--- introducao-react/src/components/List/index.jsx
+++ introducao-react/src/components/List/index.jsx
@@ -3,10 +3,10 @@ import { ListItem } from "./components/ListItem.jsx";
 export default function List( {nome, items} ) {
     return (
         <div>
-            <h2>Lista de:{nome}</h2>
+            <h2>Lista de: {nome}</h2>
             <ul>
                 {items.map((value) =>(
-                    <ListItem item={value}/>
+                    <ListItem key={index} item={value}/>
                 ))}
             </ul>
         </div>
